8fa52520200624b172b61d3f0d1c87f1eb2a12e3
Diederik Krols <diederik_krols@hotmail.com>
2016-02-20 07:20:32 +0100

    LEDBar test. Firmware upgrade required.

--- XamlBrewer.IoT.GrovePiSample/ViewModels/Sensors/LedBar.cs
+++ XamlBrewer.IoT.GrovePiSample/ViewModels/Sensors/LedBar.cs
@@ -10,9 +10,13 @@ namespace XamlBrewer.IoT.Sensors
         public LedBar()
         {
             ImagePath = "ms-appx:///Assets/Sensors/LedBar.jpg";
-            TestDescription = "Does not work on my machine ...";
+            TestDescription = "LED Bar will run through different states for about a minute.";
         }
 
+        /// <summary>
+        /// Tries some calls to get LEDs blinking.
+        /// </summary>
+        /// <remarks>Requires recent firmware on the GrovePi, which I don't have...</remarks>
         public override async Task Test()
         {
             var blinky = DeviceFactory.Build.BuildLedBar(Pin.DigitalPin4);
@@ -23,25 +27,22 @@ namespace XamlBrewer.IoT.Sensors
             }
 
             blinky.Initialize(GrovePi.Sensors.Orientation.RedToGreen);
-            await Task.Delay(TimeSpan.FromSeconds(.5));
-
-            // var board = DeviceFactory.Build.GrovePi();
-
-            // board.PinMode(Pin.DigitalPin4, PinMode.Output);
-            // board.DigitalWrite(Pin.DigitalPin4, 255);
+            var board = DeviceFactory.Build.GrovePi();
+            board.PinMode(Pin.DigitalPin4, PinMode.Output);
 
             for (var i = 1; i < 10; i++)
             {
                 try
                 {
-                    // blinky.SetLed(0, i, GrovePi.Sensors.SensorStatus.On);
+                    blinky.SetLed(0, (byte)i, GrovePi.Sensors.SensorStatus.On);
+                    await Task.Delay(TimeSpan.FromSeconds(1));
                     blinky.ToggleLed((byte)i);
-                    // blinky.SetLevel((byte)i);
-                    // board.DigitalWrite(Pin.DigitalPin4, (byte)i);
-
-
-                    await Task.Delay(TimeSpan.FromSeconds(.2));
-                    State = i.ToString();
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    blinky.SetLevel((byte)i);
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    board.DigitalWrite(Pin.DigitalPin4, (byte)i);
+                    await Task.Delay(TimeSpan.FromSeconds(1));
+                    State = "Level " + i.ToString();
                 }
                 catch (Exception ex)
                 {
